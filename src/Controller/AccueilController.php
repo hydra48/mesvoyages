280c088a33e3956dec7c0ee6124cc7b1f602c48f
joueu <joueu@LAPTOP-6RES51A3>
2022-09-29 15:35:16 +0200

    phase 1 : creation du projet

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author joueu
+ */
+class AccueilController extends AbstractController {
+    //put your code here
+    /**
+     * 
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index() : Response{
+        return $this->render('pages/accueil.html.twig');
+    }
+}
